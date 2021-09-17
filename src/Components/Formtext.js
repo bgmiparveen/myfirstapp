afd887fc192dac4bcd770ae852fb7cef149d1e26
parveen <bgmiparveen@gmail.com>
2021-09-17 08:57:13 +0530

    new line word counter bug fix

--- src/Components/Formtext.js
+++ src/Components/Formtext.js
@@ -53,7 +53,7 @@ export default function Formtext(props) {
        </div>
 
 <h2>Your text summery</h2>
-<p>{text.split(" ").filter((element)=>{return element.length!==0}).length} words and {text.length} characters </p>
+<p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words and {text.length} characters </p>
 <p> {0.008*text.split(" ").filter((element)=>{return element.length!==0}).length} Minutes to read the text </p>
 <h2>Preview</h2>
 <p>{text} </p>
